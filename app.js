db62729d6111e3c6f8995817e56ec5cc20db4fee
Abdul Kareem <abd.khaja@gmail.com>
2018-02-28 13:43:27 -0800

    implement route handler i.e. /createNewEntry for POST request originating from home / movies entry page. Implement views for home page and entry result

--- app.js
+++ app.js
@@ -13,8 +13,18 @@ app.set('views', __dirname + '/views');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (request, response) => {
-  response.send('You are on your home page');
+  // response.send('You are on your home page');
+  response.render('home');
 })
+
+app.post('/createNewEntry', (request, response) => {
+  //let movieName = req.body.
+  // console.log('Entered in createNewEntry');
+  console.log('---------------request: ', request);
+  console.log('---------------request.body: ', request.body);
+  let movieName = request.body.movieName;
+  response.render('entryResult', {'key1': movieName});
+});
 
 app.use((request, response) => {
   response.send('This is default routing page');
