c5f53b3920fdd77ded6eac9a7a90a34f7edb7354
Abdul Kareem <abd.khaja@gmail.com>
2018-02-28 16:15:44 -0800

    implement create operation for a new entry

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const nunjucks = require('nunjucks');
 const bodyParser = require('body-parser');
 
 const app = express();
-const MongoClient = mongodb.MongoClient();
+const MongoClient = mongodb.MongoClient;
 
 app.engine('html', templateEngines.nunjucks);
 app.set('view engine', 'html');
@@ -15,15 +15,62 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (request, response) => {
   // response.send('You are on your home page');
   response.render('home');
-})
+});
+
+// MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+//   if (err) {
+//     console.log('connection refused');
+//   }
+//   else {
+//     console.log('connection established');
+//
+//     app.get('/', (request, response) => {
+//       console.log('router invoked');
+//       let db = client.db('players');
+//       db.collection('test').find({}).toArray((err, docs) => {
+//         if (err) {
+//           console.log('couldnt pass into collection');
+//         }
+//         else {
+//           console.log('got into find\n');
+//           //response.render('names', {'names': docs})
+//           resonse.send('done dona done');
+//         }
+//       });
+//       //db.close();
+//     });
+//   }
+// });
 
 app.post('/createNewEntry', (request, response) => {
-  //let movieName = req.body.
-  // console.log('Entered in createNewEntry');
-  console.log('---------------request: ', request);
+  // console.log('---------------request: ', request);
   console.log('---------------request.body: ', request.body);
-  let movieName = request.body.movieName;
-  response.render('entryResult', {'key1': movieName});
+  let title = request.body.title;
+  let year = request.body.year;
+  let imdb = request.body.imdb;
+  console.log('movieName is: ', title);
+  console.log('movieYear is: ', year);
+  console.log('Type of: ', typeof year);
+
+  if ( title == '' || year == '' || imdb == '' ) {
+    response.render('failedToAdd', {'value': 'One or more field left missing'});
+  }
+
+  MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+
+    if (err) {
+      console.log('cannot connect');
+    }
+
+    let db = client.db('video');
+    let ackn = db.collection('movies').insertOne({'title': title, 'year': year, 'imdb': imdb});
+
+    if (ackn.acknowledge == 'true') {
+      //response.render('entryResult', {'key1': title});
+      response.send('Added');
+    }
+  });
+
 });
 
 app.use((request, response) => {
